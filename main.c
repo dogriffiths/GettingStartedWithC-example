25740a0281dcae7e0a6f8722f51e8d0a62897dae
davidg <dogriffiths@gmail.com>
2022-05-03 15:22:18 +0100

    Changes: Tue  3 May 2022 15:22:18 BST

--- main.c
+++ main.c
@@ -1,4 +1,5 @@
 #include <stdio.h>
+#include <string.h>
 
 int main() {
 
@@ -6,6 +7,10 @@ int main() {
 
     char name[100];
     fgets(name, 100, stdin);
+
+    int pos = strcspn(name, "\n");
+
+    name[pos] = '\0';
 
     printf("Welcome, %s!\n", name);
 
